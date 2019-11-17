132a97c3bf199d4a5b4cc766a03fe6e537d87efa
rachfuerst <rach.fuerst@gmail.com>
2019-11-17 14:58:41 -0600

    sinatra setup with notes

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
-require 'sinatra'
-class App < Sinatra::Base
+# This is the application controller (from MVC)
+require 'sinatra' # Require the Sinatra gem when this file is run
+class App < Sinatra::Base # Make the App class a subclass of Sinatra Base
+#any instance of our class App will have all the functionality of the Sinatra class.
 
-  get '/' do 
-    "Hello, world!"
+  get '/' do # This method responds to a get request on the root resource
+    "Hello, world!" # Displays "Hello world!" In the browser
   end
 
 end
